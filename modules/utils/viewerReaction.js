23b4d7006ce34f59a9c86df689496962b98c6f2b
DrixnFS <krzyzanek.martin@gmail.com>
2019-10-31 15:13:27 +0100

    Removed useless files

--- modules/utils/viewerReaction.js
+++
@@ -1,7 +0,0 @@
-module.exports = async (client, channel)=>{
-    var text = `**if you** ***UNDERSTAND*** **the** ***RULES*** **and** ***ACCEPT*** **them, click** ***ACCEPT EMOTE*** **to use this discord to its full potentional**`;
-    var message = channel.messages.find(m => m.content === text);
-    if(message) message.delete();
-    let msg = await channel.send(text);
-    await msg.react('639096098765537299');
-};
